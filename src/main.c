5e0d21bd5aaff1db4c6b68c1c1d0f11eccf4cfc0
Alexei Colin <ac@alexeicolin.com>
2017-07-03 23:13:18 -0400

    send test packets to radio via uart link

--- src/main.c
+++ src/main.c
@@ -6,9 +6,14 @@
 #include <libmsp/clock.h>
 #include <libmsp/gpio.h>
 #include <libmsp/periph.h>
+#include <libmsp/uart.h>
+#include <libmsp/sleep.h>
 #include <libio/console.h>
 
 #include "pins.h"
+
+#define MAX_PAYLOAD_LEN 25
+uint8_t pkt[1 + MAX_PAYLOAD_LEN];
 
 int main() {
     msp_watchdog_disable();
@@ -20,6 +25,28 @@ int main() {
 
     INIT_CONSOLE();
     LOG("CAPYBARA v1.0\r\n");
+
+    msp_uart_open();
+
+    uint8_t len = 1;
+
+    while (1) {
+        msp_sleep(16000);
+
+        if (len == MAX_PAYLOAD_LEN)
+            len = 1;
+
+        pkt[0] = len;
+        for (int i = 1; i < len + 1; ++i)
+            pkt[i] = '0' + (i % 10);
+
+        msp_uart_send_sync(pkt, len + 1);
+
+        ++len;
+
+    }
+
+    msp_uart_close();
 
     while(1) {
         __bis_SR_register(LPM4_bits);
